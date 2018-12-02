2d9f6c31765aa58a74df1b6a3345894b5d3d63ab
马涛 <iwater@gmail.com>
2018-12-02 17:15:13 +0800

    max_dbs_open = 500

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,15 @@
 
 FROM node:10-alpine
-LABEL maintainer "Scott Mathieson <scttmthsn@gmail.com>"
+LABEL maintainer "iwater <iwater@gmail.com>"
 
-RUN apk --update --no-cache add curl g++ make python && \
+RUN apk --update --no-cache add bash curl g++ make python && \
 	npm install --global --unsafe-perm=true pouchdb-server && \
 	rm -rf /root/.[^.]* && \
 	apk del g++ make python
 
+ADD node_modules/lru-cache /usr/local/lib/node_modules/lru-cache
+ADD node_modules/yallist /usr/local/lib/node_modules/yallist
+ADD create-or-delete-dbs.js /usr/local/lib/node_modules/pouchdb-server/node_modules/express-pouchdb/lib/create-or-delete-dbs.js
 ADD add-admin.sh /bin/add-admin
 ADD add-user.sh /bin/add-user
 ADD lib.sh /bin/lib.sh
---
+++ create-or-delete-dbs.js
@@ -0,0 +1,60 @@
+'use strict';
+
+// unified access to creating/deleting PouchDB databases,
+// to re-use PouchDB objects and avoid race conditions
+
+var Promise = require('pouchdb-promise');
+var LRU = require("lru-cache");
+
+const options = {
+        max: 500,
+        maxAge: 1000 * 60 * 60 * 6,
+        dispose: function (key, n) {
+          n.close();
+        },
+      };
+
+var promiseChain = Promise.resolve();
+
+// do all creations/deletions in a single global lock
+// obviously this makes this operation slow... but hopefully you aren't
+// creating and destroying a lot of databases all the time
+function doSequentially(fun) {
+  promiseChain = promiseChain.then(fun);
+  return promiseChain;
+}
+
+function getOrCreateDB(PouchDB, dbName) {
+  var map = PouchDB.__dbCacheMap;
+  if (!map) {
+    // cache DBs to avoid costly re-allocations
+    map = PouchDB.__dbCacheMap = new LRU(options);
+  }
+  return doSequentially(function () {
+    var db = map.get(dbName);
+    if (db) {
+      return db;
+    }
+    db = new PouchDB(dbName);
+    map.set(dbName, db);
+    return db;
+  });
+}
+
+function deleteDB(PouchDB, dbName, opts) {
+  return doSequentially(function () {
+    var dbCache = PouchDB.__dbCacheMap;
+    var db = dbCache && dbCache.get(dbName);
+    if (!db) {
+      // if the db wasn't cached, we may still need to destroy it
+      // if it's saved to disk
+      return PouchDB.destroy(dbName, opts);
+    }
+    // if this db was cached, we need to remove it from the cache immediately
+    dbCache.del(dbName);
+    return db.destroy(opts);
+  });
+}
+
+exports.getOrCreateDB = getOrCreateDB;
+exports.deleteDB = deleteDB;
